a831508568af81d9bdac672f53afbf3e05c25eeb
Moore <ktmoore1@grsla40011739.ndc.nasa.gov>
2013-02-20 16:52:46 -0500

    Small fix for Cython build on Mac

--- src/pyV3D/build_cython.py
+++ src/pyV3D/build_cython.py
@@ -4,10 +4,12 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from numpy.distutils.misc_util import get_numpy_include_dirs
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("pyV3D", ["pyV3D.pyx", "wv.c"])]
+    ext_modules = [Extension("pyV3D", ["pyV3D.pyx", "wv.c"])],
+    include_dirs = get_numpy_include_dirs(),
 )
     
 #setup(

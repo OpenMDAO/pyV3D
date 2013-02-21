d994b66dd1930a5ff60ce97bdd652f12c0124d52
Bret Naylor <bret@Bret-Naylors-MacBook-Pro.local>
2013-02-21 09:50:50 -0500

    interim commit

--- src/pyV3D/wvserver/js/controllers.js
+++ src/pyV3D/wvserver/js/controllers.js
@@ -38,38 +38,5 @@ function CommitsCtrl($scope, $http) {
 CommitsCtrl.$inject = ['$scope', '$http'];
 
 
-function CommitCtrl($scope, $http, $routeParams) {
-    $http.get('/hosts/'+$routeParams.commit_id).
-        success(function(data, status, headers, config) {
-            $scope.host_tests = data;
-        }).
-        error(function(data, status, headers, config) {
-            console.log("ERROR while getting /hosts/"+$routeParams.commit_id);
-        });
-
-    $scope.outcome = function(test) {
-        if (test.fails > 0 || test.passes == 0) {
-            return "error";
-        }
-        else if (test.skips > 0) {
-            return "info";
-        }
-        return "success";
-    };
-}
-CommitCtrl.$inject = ['$scope', '$http', '$routeParams', '$location'];
-
-
-function TestCtrl($scope, $http, $routeParams) {
-    $http.get('/results/'+$routeParams.host+'/'+$routeParams.commit_id).
-        success(function(data, status, headers, config) {
-            $scope.test = data;
-        }).
-        error(function(data, status, headers, config) {
-            console.log("ERROR while getting '/test/'"+$routeParams.host+"/"+$routeParams.commit_id);
-        });
-
-}
-TestCtrl.$inject = ['$scope', '$http', '$routeParams'];
 
 

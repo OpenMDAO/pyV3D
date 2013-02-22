2f44c1b66fbf2aac3123f757b318335cbc48bffd
Bret Naylor <naylor.b@gmail.com>
2013-02-22 15:52:43 -0500

    interim

--- src/pyV3D/wvserver.py
+++ src/pyV3D/wvserver.py
@@ -6,8 +6,8 @@ from tornado.web import RequestHandler, StaticFileHandler
 
 from argparse import ArgumentParser
 
-from pyV3D import WV_Wrapper
-from numpy import array, float32, int32, uint8
+from pyV3D.pyV3D import WV_Wrapper
+from numpy import array, float32, float64, int32, uint8
 
 debug = True
 
@@ -29,78 +29,6 @@ def get_argument_parser():
     return parser
 
 
-def create_geom():
-
-    myWV = WV_Wrapper()
-
-    eye    = array([1.0, 0.0, 7.0], dtype=float32)
-    center = array([0.0, 0.0, 0.0], dtype=float32)
-    up     = array([0.0, 1.0, 0.0], dtype=float32)
-
-    myWV.createContext(0, 30.0, 1.0, 10.0, eye, center, up)
-
-    # box
-    # v6----- v5
-    # /| /|
-    # v1------v0|
-    # | | | |
-    # | |v7---|-|v4
-    # |/ |/
-    # v2------v3
-    #
-    # vertex coords array
-    vertices = [
-        1, 1, 1, -1, 1, 1, -1,-1, 1, 1,-1, 1, # v0-v1-v2-v3 front
-        1, 1, 1, 1,-1, 1, 1,-1,-1, 1, 1,-1, # v0-v3-v4-v5 right
-        1, 1, 1, 1, 1,-1, -1, 1,-1, -1, 1, 1, # v0-v5-v6-v1 top
-       -1, 1, 1, -1, 1,-1, -1,-1,-1, -1,-1, 1, # v1-v6-v7-v2 left
-       -1,-1,-1, 1,-1,-1, 1,-1, 1, -1,-1, 1, # v7-v4-v3-v2 bottom
-        1,-1,-1, -1,-1,-1, -1, 1,-1, 1, 1,-1 ] # v4-v7-v6-v5 back
-
-    # normal array
-    normals = [
-        0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, # v0-v1-v2-v3 front
-        1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, # v0-v3-v4-v5 right
-        0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, # v0-v5-v6-v1 top
-       -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, # v1-v6-v7-v2 left
-        0,-1, 0, 0,-1, 0, 0,-1, 0, 0,-1, 0, # v7-v4-v3-v2 bottom
-        0, 0,-1, 0, 0,-1, 0, 0,-1, 0, 0,-1 ] # v4-v7-v6-v5 back
-
-    # color array
-    colors = [
-        0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255, # v0-v1-v2-v3
-        255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0, # v0-v3-v4-v5
-        0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, # v0-v5-v6-v1
-        255, 255, 0, 255, 255, 0, 255, 255, 0, 255, 255, 0, # v1-v6-v7-v2
-        255, 0, 255, 255, 0, 255, 255, 0, 255, 255, 0, 255, # v7-v4-v3-v2
-        0, 255, 255, 0, 255, 255, 0, 255, 255, 0, 255, 255] # v4-v7-v6-v5
-
-    # index array
-    indices = [
-        0, 1, 2, 0, 2, 3, # front
-        4, 5, 6, 4, 6, 7, # right
-        8, 9,10, 8,10,11, # top
-       12,13,14, 12,14,15, # left
-       16,17,18, 16,18,19, # bottom
-       20,21,22, 20,22,23 ] # back
-
-    vertices = array(vertices, dtype=float32)
-    indices = array(indices, dtype=int32)
-    colors = array(colors, dtype=uint8)
-    normals = array(normals, dtype=float32)
-
-    myWV.add_GPrim_solid("MyBox", vertices, indices, colors, normals,
-                         shading=True, orientation=True)
-
-    buf = 146*' '
-    wsi = wsi_server()
-    #myWV.send_GPrim(wsi, buf, 1, send_binary_data)
-    #myWV.send_GPrim(wsi, buf, 0, send_binary_data)
-    myWV.send_GPrim(wsi, buf, -1, send_binary_data)
-
-    myWV.remove_GPrim(0)
-    
-    return myWV
 
 # Determining size of buf for websockets:
 #    define MAX_MUX_RECURSION 2
@@ -110,29 +38,6 @@ def create_geom():
 #                             LWS_SEND_BUFFER_POST_PADDING]
 #
 # so -> 4 + 10 + 2*2 + 128 + 1 = 147
-
-def send_binary_data(wsi, buf, ibuf):
-    print "In send_binary_data"
-    print "length", len(buf)
-    print "buffer", [buf[i] for i in range(0, ibuf)]
-    print ibuf
-    wsi.check()
-    wsi.write_to_file('cube.bin', buf)
-    
-    return 0
-
-class wsi_server(object):
-    
-    def check(self):
-        print "Hello from the Server"
-        
-    def write_to_file(self, name, buf):
-        ''' Writes the binary data to a file
-        '''
-        
-        with open(name, 'wb') as out:
-            out.write(buf)
-
 
 
 class WShandler(websocket.WebSocketHandler):
@@ -144,7 +49,7 @@ class WShandler(websocket.WebSocketHandler):
     
     def open(self):
         print "WebSocket opened"
-        create_geom()
+        self.create_geom()
 
     def on_message(self, message):
         print "got message: %s" % message
@@ -159,6 +64,89 @@ class WShandler(websocket.WebSocketHandler):
             if proto in self._subprotos:
                 return proto
         return None
+
+    def send_binary_data(self, wsi, buf, ibuf):
+        print "In send_binary_data"
+        print "length", len(buf)
+        print "buffer", [buf[i] for i in range(0, ibuf)]
+        print ibuf
+        #wsi.check()
+        #wsi.write_to_file('cube.bin', buf)
+        self.write_message(buf, binary=True)
+
+        self.myWV.remove_GPrim(0)
+        
+        return 0
+
+    def create_geom(self):
+
+        myWV = WV_Wrapper()
+
+        eye    = array([1.0, 0.0, 7.0], dtype=float32)
+        center = array([0.0, 0.0, 0.0], dtype=float32)
+        up     = array([0.0, 1.0, 0.0], dtype=float32)
+
+        myWV.createContext(0, 30.0, 1.0, 10.0, eye, center, up)
+
+        # box
+        # v6----- v5
+        # /| /|
+        # v1------v0|
+        # | | | |
+        # | |v7---|-|v4
+        # |/ |/
+        # v2------v3
+        #
+        # vertex coords array
+        vertices = [
+            1, 1, 1, -1, 1, 1, -1,-1, 1, 1,-1, 1, # v0-v1-v2-v3 front
+            1, 1, 1, 1,-1, 1, 1,-1,-1, 1, 1,-1, # v0-v3-v4-v5 right
+            1, 1, 1, 1, 1,-1, -1, 1,-1, -1, 1, 1, # v0-v5-v6-v1 top
+           -1, 1, 1, -1, 1,-1, -1,-1,-1, -1,-1, 1, # v1-v6-v7-v2 left
+           -1,-1,-1, 1,-1,-1, 1,-1, 1, -1,-1, 1, # v7-v4-v3-v2 bottom
+            1,-1,-1, -1,-1,-1, -1, 1,-1, 1, 1,-1 ] # v4-v7-v6-v5 back
+
+        # normal array
+        normals = [
+            0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, # v0-v1-v2-v3 front
+            1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, # v0-v3-v4-v5 right
+            0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, # v0-v5-v6-v1 top
+           -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, # v1-v6-v7-v2 left
+            0,-1, 0, 0,-1, 0, 0,-1, 0, 0,-1, 0, # v7-v4-v3-v2 bottom
+            0, 0,-1, 0, 0,-1, 0, 0,-1, 0, 0,-1 ] # v4-v7-v6-v5 back
+
+        # color array
+        colors = [
+            0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255, # v0-v1-v2-v3
+            255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0, # v0-v3-v4-v5
+            0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, # v0-v5-v6-v1
+            255, 255, 0, 255, 255, 0, 255, 255, 0, 255, 255, 0, # v1-v6-v7-v2
+            255, 0, 255, 255, 0, 255, 255, 0, 255, 255, 0, 255, # v7-v4-v3-v2
+            0, 255, 255, 0, 255, 255, 0, 255, 255, 0, 255, 255] # v4-v7-v6-v5
+
+        # index array
+        indices = [
+            0, 1, 2, 0, 2, 3, # front
+            4, 5, 6, 4, 6, 7, # right
+            8, 9,10, 8,10,11, # top
+           12,13,14, 12,14,15, # left
+           16,17,18, 16,18,19, # bottom
+           20,21,22, 20,22,23 ] # back
+
+        vertices = array(vertices, dtype=float64)
+        indices = array(indices, dtype=int32)
+        colors = array(colors, dtype=uint8)
+        normals = array(normals, dtype=float32)
+
+        myWV.add_GPrim_solid("MyBox", vertices, indices, colors, normals,
+                             shading=True, orientation=True)
+
+        self.myWV = myWV
+        
+        buf = 147*' '
+        myWV.send_GPrim(self, buf, -1, self.send_binary_data)
+
+
 
 # class MainHandler(RequestHandler):
 #     def get(self):

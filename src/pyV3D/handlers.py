80a14bf075cd92ba54108da9d79d59d313bd078d
Bret Naylor <naylor.b@gmail.com>
2013-04-08 15:51:26 -0400

    fixed typo

--- src/pyV3D/handlers.py
+++ src/pyV3D/handlers.py
@@ -46,7 +46,7 @@ class WSHandler(websocket.WebSocketHandler):
             super(WSHandler, self)._execute(transforms, *args, **kwargs)
         except Exception as err:
             ERROR("%s" % err)
-        self._prototype = None
+        self._protocol = None
         return None
 
     def open(self):
@@ -59,14 +59,14 @@ class WSHandler(websocket.WebSocketHandler):
             # another protocol, e.g., pyv3d-bin-1.0 and pyv3d-txt-1.0
                 self.subhandler = self.subhandlers.get(tuple(args))
                 if self.subhandler is not None:
-                    DEBUG("subhandler already existed, adding protocol %s to it." % self._prototype)
+                    DEBUG("subhandler already existed, adding protocol %s to it." % self._protocol)
 
                 if self.subhandler is None:
                     DEBUG("creating a new subhandler for %s" % args)
                     # try to create a subhandler matching the given protocol.  Take
                     # the first one that succeeds.  This means that subhandlers MUST
                     # raise an exception if they are not able to successfully construct themselves.
-                    for klass in self.protocols.get(self._prototype, []):
+                    for klass in self.protocols.get(self._protocol, []):
                         DEBUG("trying to create a ",klass)
                         try:
                             self.subhandler = klass(self, *args, **kwargs)
@@ -102,20 +102,20 @@ class WSHandler(websocket.WebSocketHandler):
                         del self.subhandlers[key]
                         break
 
-        DEBUG("WebSocket closed (proto=%s)" % self._prototype)
+        DEBUG("WebSocket closed (proto=%s)" % self._protocol)
 
     def select_subprotocol(self, subprotocols):
         DEBUG("in select_subprotocol")
         try:
             for p in subprotocols:
                 if p in self.protocols:
-                    self._prototype = p
+                    self._protocol = p
                     DEBUG("matched subproto %s" % p)
                     return p
             DEBUG("returning None for subproto choices: %s" % subprotocols)
         except Exception as err:
             ERROR("%s" % err)
-        self._prototype = None
+        self._protocol = None
         return None
 
     def find_file(self, fname):
@@ -128,20 +128,26 @@ class WSHandler(websocket.WebSocketHandler):
 
 
 class SubHandler(object):
+    """This class exists so that we can have the same handler for multiple websocket
+    subprotocols.  This makes it easier to write pyV3D viewers because pyV3D requires
+    a text protocol and a binary protocol.  Each subprotocol is handled by a separate
+    websocket handler in tornado.  Instances that inherit from this class will receive
+    callbacks from both websocket handlers.
+    """
     def __init__(self):
         self.handlers = {}  # need real websocket handler(s) to send msgs
 
     def on_message(self, handler, message):
-        DEBUG("websocket subhandler got message for protocol (%s): %s" % (handler._prototype, message))
+        DEBUG("websocket subhandler got message for protocol (%s): %s" % (handler._protocol, message))
 
     def on_close(self, handler):
-        DEBUG("WebSocket subhandler closed for protocol %s" % handler._prototype)
+        DEBUG("WebSocket subhandler closed for protocol %s" % handler._protocol)
 
     def open(self, handler):
-        DEBUG("WebSocket subhandler opened for protocol %s" % handler._prototype)
-        if handler._prototype in self.handlers:
-            raise RuntimeError("this subhandler already has a handler for protocol %s" % handler._prototype)
-        self.handlers[handler._prototype] = handler
+        DEBUG("WebSocket subhandler opened for protocol %s" % handler._protocol)
+        if handler._protocol in self.handlers:
+            raise RuntimeError("this subhandler already has a handler for protocol %s" % handler._protocol)
+        self.handlers[handler._protocol] = handler
 
 
 
@@ -150,6 +156,7 @@ class WV_ViewHandler(SubHandler):
         super(WV_ViewHandler, self).__init__()
 
         self.wv = WV_Wrapper()
+        # TODO: make this buffer internal to WV_Wrapper
         self.buf = self.wv.get_bufflen()*'\0'
 
     @staticmethod
@@ -183,7 +190,7 @@ class WV_ViewHandler(SubHandler):
     def open(self, handler):
         super(WV_ViewHandler, self).open(handler)
         try:
-            if handler._prototype == 'pyv3d-bin-1.0':
+            if handler._protocol == 'pyv3d-bin-1.0':
                 self.create_geom()
                 self.send_geometry(first=True)
         except Exception as err:
